09152fc4a3c2610194413ea10ca2c603e56b9a0a
juanfel <jf3dsaber@gmail.com>
2016-07-29 00:22:53 -0400

    Cambios para obtener letras

--- scrapper-test.py
+++ scrapper-test.py
@@ -5,3 +5,22 @@ def html_requests_test():
 def html_tree_test():
     assert scrapper.tree is not None
     assert len(scrapper.artistas) > 1
+    print(scrapper.artistas[0].items())
+def get_lyric_page_name_test():
+    assert len(scrapper.lyrics_page_names) > 1
+    print(scrapper.lyrics_page_names[0])
+def get_lyric_page_test():
+    pagina = "http://www.musica.com/letras.asp?letras=3573"
+    lyrics_pages = scrapper.obtener_letras_pagina(pagina)
+    
+    print(lyrics_pages[0].items())
+    assert lyrics_pages is not None
+    assert len(lyrics_pages) > 0
+def get_lyrics_test():
+    pagina = "http://www.musica.com/letras.asp?letra=801505"
+    letra = scrapper.obtener_letra(pagina)
+
+    print(letra)
+    assert letra is not None
+def all_lyrics_test():
+    assert len(scrapper.lyrics) > len(scrapper.artistas)
--- scrapper.py
+++ scrapper.py
@@ -1,8 +1,59 @@
 import requests
 from lxml import html
+from lxml import etree
 
-page = requests.get('http://www.musica.com/letras.asp?letras=canciones')
+try:
+    page = requests.get('http://www.musica.com/letras.asp?letras=canciones')
+except Exception as error:
+    print("Problem connecting to page", error)
+    raise
 tree = html.fromstring(page.content)
 
 xpath_all_artists = "/html/body/table[1]/tr/td/table/tr[3]/td/table/tr/td[3]/table/tr[3]/td/table/tr/td/table/tr[3]/td/table/tr[3]/td/table/tr/td[1]/p/font/a[contains(@href,'letras.asp?letras')]"
 artistas = tree.xpath(xpath_all_artists)
+
+
+def obtener_paginas_artistas(artistas):
+    "Obtiene las paginas de los artistas a partir de la lista generada por el xpath de la pagina principal"
+    page_names = []
+    for artista in artistas:
+        info_artista = artista.items()[0][1]
+        page_names.append("http://www.musica.com/" + info_artista)
+    return page_names
+
+lyrics_page_names = obtener_paginas_artistas(artistas)
+
+def obtener_letras_paginas_artistas(paginas_artistas):
+    "Obtiene las letras de cada pagina obtenida como string"
+    letras = []
+    for artista in paginas_artistas:
+        print("obteniendo letras de: " + artista)
+        for element_letra in obtener_letras_pagina(artista):
+            pagina_letra = "http://www.musica.com/" + element_letra.values()[0]
+            letra = obtener_letra(pagina_letra)
+            if letra is not None:
+                letras.append(letra)
+    return letras
+def obtener_letras_pagina(pagina_artista):
+    "Obtiene todas las letras de la pagina de un artista dado. La pagina es un string"
+    letras = []
+    content_pagina = requests.get(pagina_artista).content
+    tree_pagina = html.fromstring(content_pagina)
+    xpath_letras = "/html/body/table[1]/tr/td/table/tr[3]/td/table/tr/td[3]/table/tr[3]/td/table/tr/td/table/tr[4]/td/table/tr[1]/td/table/tr/td[1]/p/font/a[contains(@href,'letras.asp?letra=')]"
+    letras = tree_pagina.xpath(xpath_letras)
+    return letras
+
+def obtener_letra(pagina_letra):
+    "Obtiene la letra que se encuentra en una pagina"
+    content_pagina = requests.get(pagina_letra).content
+    tree_pagina = html.fromstring(content_pagina)
+    xpath_letra = "/html/body/table[1]/tr/td/table/tr[3]/td/table/tr/td[3]/table/tr[3]/td/table/tr/td/table/tr[4]/td/table/tr[4]/td/table/tr/td[2]/p"
+
+    try:
+        letra_paragraph = tree_pagina.xpath(xpath_letra)
+        letra = etree.tostring(letra_paragraph[0], encoding="unicode", method="text")
+    except:
+        letra = None
+    return letra
+
+lyrics = obtener_letras_paginas_artistas(lyrics_page_names)
