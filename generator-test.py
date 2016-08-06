46f375569efe9ba5bafae6236ec51ab4d2c876cf
juanfel <jf3dsaber@gmail.com>
2016-08-06 01:08:59 -0400

    Genera oraciones a partir del modelo

--- generator-test.py
+++ generator-test.py
@@ -6,4 +6,23 @@ def setup_module():
 def preprocess_lyric_test():
     lyric = lyric_gen.preprocess_lyric("lyric\rlyric")
     print(lyric)
-    assert lyric == "lyric\nlyric" 
+    assert lyric == "lyric\nlyric\n" 
+def join_lyrics_test():
+    lyric_gen.join_lyrics()
+    assert lyric_gen.text == ""
+
+    lyric_gen.text_data = ["lyrics\n","data"]
+    lyric_gen.join_lyrics()
+    print(lyric_gen.text)
+    assert lyric_gen.text == "lyrics\ndata"
+def get_lyrics_test():
+    lyric_gen.text_data = []
+    lyric_gen.get_lyrics_from_db(limit = 100)
+
+    assert len(lyric_gen.text_data) == 100
+def get_generated_sentence_test():
+    lyric_gen.join_lyrics()
+    lyric_gen.feed_lyrics_to_model()
+    sentence = lyric_gen.get_generated_sentence()
+    print(sentence)
+    assert sentence != None
--- generator.py
+++ generator.py
@@ -6,10 +6,43 @@ class LyricsGenerator:
     """Clase encargada de obtener y procesar el texto de la
     base de datos y obtener las letras nuevas.
     """
-    def preprocess_lyric(self,lyric):
+    def __init__(self):
+        self.text_data = []
+        self.lyric_collection = db_manager.LyricDatabase()
+        self.lyric_collection.connect()
+    def preprocess_lyric(self, lyric):
         """Se encarga de transformar un texto especifico para modificar
         cosas como saltos de linea, entre otros
         """
-        new_lyric = re.sub(r"\r",r"\n",lyric)
-        return new_lyric
-        
+        new_lyric = re.sub(r"\r",r"\n",lyric + "\n")
+        return new_lyric 
+    def join_lyrics(self):
+       """Une todas las letras almacenadas en el objeto
+       """
+       self.text = ''.join(self.text_data)
+    def get_lyrics_from_db(self, limit = 0):
+      """Obtiene todas las letras de la base de datos
+      hasta el limite dado y las agrega a los datos
+      """
+      lyric_iterator = self.lyric_collection.get_lyric_iterator(limit)
+      for song in lyric_iterator:
+          try:
+              lyric = song["Letra"]
+              new_song = self.preprocess_lyric(lyric)
+              self.text_data.append(new_song)
+          except:
+              print("Error agregando letra")
+              raise 
+    def feed_lyrics_to_model(self):
+       """Agrega las letras al modelo de markov
+       """
+       self.text_model = markovify.Text(self.text)
+    def get_generated_sentence(self):
+       """A partir del modelo obtiene una oracion random
+       """
+       sentence = self.text_model.make_sentence()
+       return sentence
+    def markovify_songs(limit = 0):
+       """Hace todos los pasos para obtener el modelo
+       """ 
+    
