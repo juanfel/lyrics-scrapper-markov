5621af75e48ab1597e7a9e2100ef862dd005eea6
juanfel <jf3dsaber@gmail.com>
2016-08-08 22:10:12 -0400

    Hello world simple.

---
+++ webmanager.py
@@ -0,0 +1,14 @@
+from flask_script import Manager, Server
+from webpage import app
+
+manager = Manager(app)
+
+# Turn on debugger by default and reloader
+manager.add_command("runserver", Server(
+    use_debugger = True,
+    use_reloader = True,
+    host = '0.0.0.0')
+)
+
+if __name__ == "__main__":
+    manager.run()
