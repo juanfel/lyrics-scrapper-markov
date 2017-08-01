863c2bd5a7c72d103d91a1dee87d7854976b870e
juanfel <jf3dsaber@gmail.com>
2017-08-01 03:26:19 -0400

    Agrega canciones a la base de datos.

--- top100scraper/top100scraper/pipelines.py
+++ top100scraper/top100scraper/pipelines.py
@@ -4,8 +4,12 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
-
+from top100scraper.db_manager import LyricDatabase
 
 class Top100ScraperPipeline(object):
+    """Guarda los items en la BD"""
     def process_item(self, item, spider):
+        db = LyricDatabase()
+        db.connect()
+        db.add_lyric(item['artist'], item['song'], item['text'])
         return item
