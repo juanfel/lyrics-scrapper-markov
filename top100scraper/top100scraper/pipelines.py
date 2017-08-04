a1e58cd212acb8b0238a937aaf4b0c7f2bcf6219
juanfel <jf3dsaber@gmail.com>
2017-08-03 23:22:58 -0400

    Crea clase para pipeline de tags.

--- top100scraper/top100scraper/pipelines.py
+++ top100scraper/top100scraper/pipelines.py
@@ -5,6 +5,7 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 from top100scraper.db_manager import LyricDatabase
+import pylast 
 
 class Top100ScraperPipeline(object):
     """Guarda los items en la BD"""
@@ -13,3 +14,12 @@ class Top100ScraperPipeline(object):
         db.connect()
         db.add_lyric(item['artist'], item['song'], item['text'])
         return item
+
+class GetTagsPipeline(object):
+    """Busca tags de los artistas en Last.fm.
+    Necesita las credenciales de usuario, las cuales son obtenidas desde el spider."""
+    API_KEY = '574d1c1a07bcaf2fdcf57aa75babde50'
+    SHARED_SECRET = '7fd795ca45a71f92a42b6fbbbdd8400e'
+    
+    def process_item(self, item, spider):
+        return spider
